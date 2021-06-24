e5e5e05d1fe70d1e8f901c93477931e2dd0b9833
Omkar Vishwakarma <Omkar200018@gmail.com>
2021-06-24 21:31:02 +0530

    adding static background button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const color1= document.querySelector(".color1");
 const color2= document.querySelector(".color2");
+const color3= document.querySelector(".color3");
+
 const gradientBackground = document.querySelector("body");
 const cssCode = document.querySelector(".csscode");
 
@@ -7,11 +9,21 @@ function setgb() {
     gradientBackground.style.background = "linear-gradient(to right, "+ color1.value +","+ color2.value + ")" ;
 
     cssCode.value =  gradientBackground.style.background + ";"
-    console.log(color1.value + " " + color2.value)
+    // console.log(color1.value + " " + color2.value)
+}
+
+function setSb(){
+  gradientBackground.style.background = color3.value;
+  
+  cssCode.value =  gradientBackground.style.background + ";"
+  // console.log(color1.value + " " + color2.value) 
+
 }
 
 color1.addEventListener("input",setgb);
 color2.addEventListener("input",setgb );
+color3.addEventListener("input",setSb );
+
 
 
 function copy() {
